d4e652a3d5076e6a923661dfaef62bc4e47fa01c
johnnyman727 <jonathan.mckay@students.olin.edu>
2012-12-24 22:17:11 -0800

    Fixed websocket addr

--- public/livePhoto-client.js
+++ public/livePhoto-client.js
@@ -1,5 +1,5 @@
   // var browser_socket = io.connect('http://localhost:8080');
-  var browser_socket = io.connect('192.168.2.3:8080');
+  var browser_socket = io.connect('jonamckay.com:8080');
   console.log("Starting socket client side");
   browser_socket.on('newImage', function (data) {
 
